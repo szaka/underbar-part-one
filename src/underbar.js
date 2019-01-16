ae57f0796305fbc36efc76426adf1b2d126acdb9
Shahid Zaka <szaka@sz.local>
2019-01-15 16:34:14 -0800

    commit underbar.js

--- src/underbar.js
+++ src/underbar.js
@@ -8,7 +8,7 @@
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
     /* START SOLUTION */
-
+    return val;
     /* END SOLUTION */
   };
 
@@ -27,7 +27,21 @@
   // return just the first element.
   _.first = function(array, n) {
     /* START SOLUTION */
-
+    var firstArray = [];
+    if ( n === 0) {
+      return [];
+    }
+    if ( n > array.length) {
+      return array;
+    }
+    if ( n === undefined) {
+      return array[0];
+    } else {
+      for ( var i = 0; i < n; i++ ) {
+        firstArray.push(array[i]);
+      }
+      return firstArray;
+    }
     /* END SOLUTION */
   };
 
@@ -35,7 +49,21 @@
   // last element.
   _.last = function(array, n) {
     /* START SOLUTION */
-
+    var lastArray = [];
+    if ( n === 0) {
+      return [];
+    }
+    if ( n > array.length) {
+      return array;
+    }
+    if ( n === undefined) {
+      return array[array.length-1];
+    } else {
+      for ( var i = 0; i < n; i++ ) {
+        lastArray.push(array[i]);
+      }
+      return lastArray;
+    }
     /* END SOLUTION */
   };
 
@@ -46,45 +74,137 @@
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
     /* START SOLUTION */
-
+    if (Array.isArray(collection)){
+      for (var i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    } else {
+    for (var i in collection){
+      iterator(collection[i], i, collection); 
     /* END SOLUTION */
+     }
+    }
   };
+  
+
+// if array iterator(element, index, list)
+// if object iterator(value, key, list)
+
 
   // Returns the index at which value can be found in the array, or -1 if value
   // is not present in the array.
   _.indexOf = function(array, target){
     /* START SOLUTION */
-
+    
+    for (var i = 0; i < array.length ; i++){
+      var count = 0;
+      if (array[i] === target){
+        count++
+        return i;
+      }
+    }
+     if (count === 0){
+        return -1;
+      }
     /* END SOLUTION */
   };
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
     /* START SOLUTION */
-
+    var filterArr = [];
+    for (var i = 0; i < collection.length; i++){
+    if (test(collection[i]) === true){
+      filterArr.push(collection[i])
+      }
+    }
+    return filterArr;
     /* END SOLUTION */
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     /* START SOLUTION */
-
+    var rejectArr = [];
+    for (var i = 0; i < collection.length; i++){
+    if (test(collection[i]) === false){
+      rejectArr.push(collection[i])
+      }
+    }
+    return rejectArr;
     /* END SOLUTION */
   };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array, isSorted, iterator) {
     /* START SOLUTION */
+    var iteratorArr = [];
 
-    /* END SOLUTION */
-  };
+    if (isSorted === undefined || isSorted === false){
+    var uniqArr = [];
+    var newObj = {};
+    for (var i = 0; i < array.length; i++){
+      if (newObj[array[i]] === undefined){
+        newObj[array[i]] = 1;
+      } else {
+        newObj[array[i]]++;
+      }
+    }
+    var keyArr = Object.keys(newObj); 
+    for (var k = 0; k < keyArr.length; k++){
+      uniqArr.push(Number(keyArr[k]));
+     }
+    return uniqArr;
+
+  } else if (isSorted === true) {
+    var arrayOfIndexes = [array[0]];
+    var checkValue = array[0];
+    for (var i = 0; i < array.length; i++){
+      if (checkValue !== array[i]){
+        arrayOfIndexes.push(array[i]);
+        checkValue = array[i]; //have array of indexes 
+      } 
+    }
+  }
+
+};
+
+// iteratorArr = [];
+// for loop
+
+// iteratorArr.push(iterator(array[i])) //function ===> tests array[i] against a boolean value
+// //[true, false, false ,false, false, false]
+// //run it through the first section
+// //will get [true, false]
+// //run 
+
+
+// var iterator = function(value) { return value === 1; }; //bool
+//         var numbers = [1, 2, 2, 3, 4, 4];
+// //                       [true, false, false ,false, false, false]
+// //                       [0,1,2,3,4,5]
+// //                       [1, 2, 3, 4]
+//                       [true, false]
+// //                       [0,1]
+// //                       [1,2]
+
+// //         expect(_.uniq(numbers)).to.eql([1, 2]);
+
+
+
+
 
 
   // Return the results of applying an iterator to each element.
   _.map = function(collection, iterator) {
     /* START SOLUTION */
-
+     var mapArr = [];
+     for ( var i = 0; i < collection.length; i++) {
+      mapArr.push(iterator(collection[i]));
+     }
+       return mapArr;
     /* END SOLUTION */
+
   };
 
   /*
@@ -101,7 +221,11 @@
     // values into a new array of values. _.pluck() is solved for you
     // as an example of this.
     /* START SOLUTION */
-
+      var pluckArr = [];
+      for (var i = 0; i < collection.length; i++) {
+        pluckArr.push(collection[i][key]);
+      }
+      return pluckArr;
     /* END SOLUTION */
   };
 
@@ -117,8 +241,24 @@
   //
   _.reduce = function(collection, iterator, accumulator) {
     /* START SOLUTION */
-
+    
     /* END SOLUTION */
   };
+
+//var sum = _.reduce([1, 2, 3], function(memo, num){ return memo + num; }, 0);
+//=> 6
+//  The iteratee is passed four arguments: the memo(accumulator), then the value and index (or key) of the iteration,
+// and finally a reference to the entire list.
+
+
+
+
+
+
+
+
+
+
+
 
 }());
